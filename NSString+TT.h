dc290e970942c0890d288d3b0c14cf281982546f
吴宝发 <wubaofa@szy.cn>
2018-11-22 17:46:05 +0800

    dd

---
+++ NSString+TT.h
@@ -0,0 +1,16 @@
+//
+//  NSString+TT.h
+//  Pods-TTLib_Example
+//
+//  Created by 吴宝发 on 2018/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSString (TT)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ NSString+TT.m
@@ -0,0 +1,12 @@
+//
+//  NSString+TT.m
+//  Pods-TTLib_Example
+//
+//  Created by 吴宝发 on 2018/11/22.
+//
+
+#import "NSString+TT.h"
+
+@implementation NSString (TT)
+
+@end
